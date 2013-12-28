b9cb93d2ca5ea420c7c1efa7767129af430b40fc
alansaviolobo <alansaviolobo@2190830.no-reply.drupal.org>
2013-12-28 12:01:51 +0530

    first cut of working module.

---
+++ simple-timeline-fields.tpl.php
@@ -0,0 +1,8 @@
+<div>
+  <span class="timeline-image"><?php echo $simple_timeline_image; ?></span>
+
+    <span class="timeline-content">
+        <h3 class="timeline-date"><?php echo $simple_timeline_date; ?></h3>
+        <span class="timeline-text"><?php echo $simple_timeline_text; ?></span>
+    </span>
+</div>
