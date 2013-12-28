8564c7541b9f03136ff11eaac6a7baff937ba7dc
Alan Lobo <alansaviolobo@gmail.com>
2013-12-28 12:37:30 +0530

    fixed the template and module file as per pareview.

--- simple-timeline-fields.tpl.php
+++ simple-timeline-fields.tpl.php
@@ -1,3 +1,15 @@
+<?php
+/**
+ * @file
+ * simple-timeline-fields.tpl.php
+ * Created by JetBrains PhpStorm.
+ * User: alan
+ *
+ * @var $simple_timeline_image
+ * @var $simple_timeline_date
+ * @var $simple_timeline_text
+ */
+?>
 <div>
   <span class="timeline-image"><?php echo $simple_timeline_image; ?></span>
 
